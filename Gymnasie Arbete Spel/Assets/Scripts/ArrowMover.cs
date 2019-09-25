9c605bbfe9aabfd731a6dd5e98d21f3ee91be5df
oliver176 <55237259+oliver176@users.noreply.github.com>
2019-09-25 09:00:08 +0200

    Arrow mover

--- Gymnasie Arbete Spel/Assets/Scripts/ArrowMover.cs
+++ Gymnasie Arbete Spel/Assets/Scripts/ArrowMover.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class ArrowMover : MonoBehaviour
 {
-    public Gameobject player;
+    public GameObject player;
+    private Rigidbody2D rb2D;
+    public int thrust;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void FixedUpdate()
+    {
+        rb2D.AddForce((player.transform.position - transform.position).normalized * thrust * Time.smoothDeltaTime);
     }
 }
