41e1e17bc16612d15e778057f392cc0162ecf2ed
ErkanShrerkan <43791546+ErkanShrerkan@users.noreply.github.com>
2019-09-26 12:56:21 +0200

    tdutdy

---
+++ Gymnasie Arbete Spel/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int level = 0;
+    public int xpToLevelUp = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LevelUp();
+        Debug.Log(level);
+    }
+
+    void XPToLEvelUp()
+    {
+        //xpToLevelUp 
+    }
+
+    void LevelUp()
+    {
+        level++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
