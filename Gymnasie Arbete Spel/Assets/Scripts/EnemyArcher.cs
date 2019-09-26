a5713b7205474f419639019fe40abfaa77315c98
ErkanShrerkan <43791546+ErkanShrerkan@users.noreply.github.com>
2019-09-26 13:21:18 +0200

    added 2 scripts

---
+++ Gymnasie Arbete Spel/Assets/Scripts/EnemyArcher.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyArcher : MonoBehaviour
+{
+    int currentHP;
+    public int baseHP = 90;
+    public int hpModifier = 12;
+    public float dmgModifier = 0.008f;
+    public float minDmg = 2000;
+    public float maxDmg = 3500;
+    public float speedModifier = 0;
+    public int playerLevel;
+    int enemyLevel;
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerStats playerStats = Player.GetComponent<PlayerStats>();
+        enemyLevel = playerStats.level;
+        currentHP = baseHP + (hpModifier * enemyLevel);
+        dmgModifier *= enemyLevel;
+        minDmg *= dmgModifier;
+        maxDmg *= dmgModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
