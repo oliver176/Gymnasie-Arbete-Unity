907db7073d69579794b397f3c954e4e4a3369070
oliver176 <55237259+oliver176@users.noreply.github.com>
2019-10-03 22:30:21 +0200

    Arrow rotation

--- Gymnasie Arbete Spel/Assets/Scripts/ArrowMover.cs
+++ Gymnasie Arbete Spel/Assets/Scripts/ArrowMover.cs
@@ -26,7 +26,10 @@ public class ArrowMover : DamageDealer
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 direction = player.transform.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 1000);
     }
     void FixedUpdate()
     {
