0bdf9f4777981d47fe4b0cca3a2bbbd8d4bc4af5
oliver176 <55237259+oliver176@users.noreply.github.com>
2019-10-06 00:21:05 +0200

    Animations

---
+++ Gymnasie Arbete Spel/Assets/Scripts/Warrior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warrior : MonoBehaviour
+{
+    bool withinRange = false;
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        if (withinRange) //om player inom range
+        {
+
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")  //inom collidern som representerar range
+        {
+            withinRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")  //när player lämnar collidern
+        {
+            withinRange = false;
+        }
+    }
+}
