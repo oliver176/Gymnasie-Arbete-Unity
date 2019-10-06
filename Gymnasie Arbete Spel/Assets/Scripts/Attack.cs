2a4f71e3133bb3b2077c4b987077b5cfbf644c93
ErkanShrerkan <43791546+ErkanShrerkan@users.noreply.github.com>
2019-10-06 15:06:50 +0200

    created methods for attacking

---
+++ Gymnasie Arbete Spel/Assets/Scripts/Attack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : PlayerStats
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Hit();
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Cast();
+        }
+    }
+
+    void Hit()
+    {
+        Debug.Log("HAJAAA!!!");
+    }
+
+    void Cast()
+    {
+        Debug.Log("HADOUKEN!!!");
+    }
+}
--- Gymnasie Arbete Spel/Assets/Scripts/Player.cs
+++ Gymnasie Arbete Spel/Assets/Scripts/Player.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    protected static int physSkillLevel;
+    protected static int magiSkillLevel;
+    protected static float minPhysDmg;
+    protected static float maxPhysDmg;
+    protected static float minMagiDmg;
+    protected static float maxMagiDmg;
+    protected static float physDmgModifier;
+    protected static float magiDmgModifier;
+    public static float currentXP = 0;
+    protected static float xpToLevelUp = 100;
+    protected static int xpModifierperLvl = 20;
+    protected static float playerMaxHealth;
+    protected static float playerMaxShield;
+    protected static float shieldRechargeDelay;
+    public static float playerCurrentHealth;
+    public static float playerCurrentShield;
+    public static int level = 0;
 
     // Start is called before the first frame update
     void Start()
