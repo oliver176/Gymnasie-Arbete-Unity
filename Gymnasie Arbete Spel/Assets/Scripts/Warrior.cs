9cb746934e17b70f11fc575f6b265d8ec7d9123e
oliver176 <55237259+oliver176@users.noreply.github.com>
2019-10-06 16:23:51 +0200

    tews

--- Gymnasie Arbete Spel/Assets/Scripts/Warrior.cs
+++ Gymnasie Arbete Spel/Assets/Scripts/Warrior.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Warrior : Enemy
 {
     bool withinRange = false;
+    private Animator anim;
 
     private void Start()
     {
+        anim = GetComponent<Animator>();
+
         hpModifier = 12;
         baseHP = 90 + (hpModifier * SetLevel());
         currentHP = baseHP;
@@ -28,6 +32,7 @@ public class Warrior : Enemy
         if (other.gameObject.name == "Player")  //inom collidern som representerar range
         {
             withinRange = true;
+            anim.SetBool("WarriorAttackRange", true);
         }
     }
     private void OnTriggerExit2D(Collider2D other)
@@ -35,6 +40,7 @@ public class Warrior : Enemy
         if (other.gameObject.name == "Player")  //när player lämnar collidern
         {
             withinRange = false;
+            anim.SetBool("WarriorAttackRange", false);
         }
     }
 }
