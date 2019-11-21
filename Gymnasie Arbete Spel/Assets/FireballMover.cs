83491e8dd6bed08978f7af3c69b4201fe8865d41
oliver176 <55237259+oliver176@users.noreply.github.com>
2019-11-21 13:23:27 +0100

    sd

--- Gymnasie Arbete Spel/Assets/FireballMover.cs
+++ Gymnasie Arbete Spel/Assets/FireballMover.cs
@@ -1,12 +1,23 @@
 ﻿using UnityEngine;
 
-public class FireballMover : MonoBehaviour
+public class FireballMover : CharacterController2D
 {
     public float projectileSpeed;
+    Vector2 direction;
+    bool facingRight;
 
     // Start is called before the first frame update
     private void Start()
     {
+        if (m_FacingRight)
+        {
+            facingRight = true;
+            Flip();
+        }
+        else if (!m_FacingRight)
+        {
+            facingRight = false;
+        }
     }
 
     // Update is called once per frame
@@ -16,6 +27,27 @@ public class FireballMover : MonoBehaviour
 
     private void FixedUpdate()
     {
-        transform.Translate(Vector2.left * projectileSpeed);
+        if (facingRight)
+        {
+            transform.Translate(Vector2.right * projectileSpeed);
+        }
+        else if (!facingRight)
+        {
+            transform.Translate(Vector2.left * projectileSpeed);
+        }
+    }
+    private void Flip()
+    {
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "World Collider")
+        {
+            Destroy(gameObject);
+        }
     }
 }
