b512d76467423dbb5bf5371b73a0711207a1dd1a
oliver176 <55237259+oliver176@users.noreply.github.com>
2019-11-21 14:37:16 +0100

    fireball yes

--- Gymnasie Arbete Spel/Assets/Scripts/Attack.cs
+++ Gymnasie Arbete Spel/Assets/Scripts/Attack.cs
@@ -1,19 +1,19 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Attack : PlayerStats
 {
     public GameObject Fireball;
+    private Animator anim;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.X))
         {
@@ -25,13 +25,20 @@ public class Attack : PlayerStats
         }
     }
 
-    void Hit()
+    private IEnumerator CastTimer()
+    {
+        yield return new WaitForSeconds(0.25f);
+        Instantiate(Fireball, new Vector3(transform.position.x, transform.position.y, -1), transform.rotation);
+    }
+
+    private void Hit()
     {
         Debug.Log("HAJAAA!!!");
     }
 
-    void CastFireball()
+    private void CastFireball()
     {
-        Instantiate(Fireball, new Vector3(transform.position.x, transform.position.y, -1), transform.rotation);
+        anim.SetTrigger("PlayerCast");
+        StartCoroutine("CastTimer");
     }
 }
